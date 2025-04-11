cd40bf8bbb685bf97d556a4c4056a0d9f755eb8d
samuele301197 <parisisamu32@gmail.com>
2025-04-11 15:29:17 +0200

    feat:strings

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -1,11 +1,21 @@
 // 1. Dichiarazione di variabili
 // Crea una variabile chiamata name e assegnale il tuo nome.
 // Stampa il valore di nome nella console.
-
+const name = "Samuele"
+console.log(name);
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
+const number = 32
+console.log(number);
+
+const text = "Bello"
+console.log(text);
+
+const bool = 1
+console.log(bool);
+
 
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
@@ -14,10 +24,13 @@ const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // NON MODIFICARE QUESTA RIGA
 const firstName = 'Jane'; // NON MODIFICARE QUESTA RIGA
 const lastName = 'Smith'; // NON MODIFICARE QUESTA RIGA
 
-const fullName = null;
+const fullName = firstName + " " + lastName;
 console.log(fullName); // Output: "Jane Smith"
 
 // 4. Imposta questa variabile utilizzando una proprietà
 // sulla variabile alphabet per ottenere la sua lunghezza
-const numberOfLettersInAlphabet = null;
+const numberOfLettersInAlphabet = alphabet.length;
+console.log(typeof numberOfLettersInAlphabet);
 console.log(numberOfLettersInAlphabet); // Output: 26
+
+
